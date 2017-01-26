a7be155f1f3c50a677defcaf36531b9d4f730969
win7 <limbertx@hotmail.net>
2017-01-25 23:13:07 -0400

    actualizacion de los modelos y documentacion

--- config/db.php
+++ config/db.php
@@ -3,7 +3,6 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn'	=>	'pgsql:host=localhost;port=5432;dbname=dbstudio',
-    //'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'postgres',
     'password' => '200221094',
     'charset' => 'utf8',
