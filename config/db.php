4c05b262d3f34b3d25d4f937cc6655a0963ec962
Limbert <limbertx@hotmail.com>
2017-12-07 00:44:38 -0400

    actualizacion de configuracion de bd server

--- config/db.php
+++ config/db.php
@@ -1,12 +1,13 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn'	=>	'pgsql:host=localhost;port=5432;dbname=dbisphoto',
-    //'dsn'	=>	'pgsql:host=ec2-54-243-185-123.compute-1.amazonaws.com;port=5432;dbname=d842vdms92k64t',
+    //'dsn'=>'pgsql:host=localhost;port=5432;dbname=dbisphoto',
+
+    'dsn'=>'pgsql:host=ec2-54-235-219-113.compute-1.amazonaws.com;port=5432;dbname=d43bk4fjq9s3o3',
     //'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
     //'username' => 'wmsjuhobreunqu',
-    'username' => 'odoo', //en linux
-    'password' => 'odoo', //en linux
+    'username' => 'nrqvmyyobolxjj',
+    'password' => 'e31b6737495ec7705ad14f2fb7394185fd9b0dc7bc734bf26ea24a3704600a34',
     //'username' => 'openpg', // windows
     //'password' => 'openpgpwd',  	// windows
     //'password' => '2741fa4bef9369a9a825856c208f7a7e41f1105d479a82e9bfe04b4d36c61dc7',
